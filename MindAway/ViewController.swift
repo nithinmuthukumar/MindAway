530547f400a65df8b60af20ed1eebf7219af6bb7
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-09-14 01:23:54 -0400

    Initial Commit

---
+++ MindAway/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MindAway
+//
+//  Created by nithin muthukumar on 2019-09-14.
+//  Copyright © 2019 nithin muthukumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
